5479c66430c4147b6b8f7294dc3d50a275a36474
Ilya <ilya@cherezov.space>
2024-06-06 21:57:51 +0200

    enable security.polkit

--- configuration.nix
+++ configuration.nix
@@ -93,6 +93,8 @@
     iosevka
   ];
 
+  security.polkit.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.fish.enable = true;
