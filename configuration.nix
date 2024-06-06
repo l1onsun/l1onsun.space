f16601294f5c639248bc6ffcb02a2644d9f7bf7a
Ilya <ilya@cherezov.space>
2024-06-06 22:09:04 +0200

    many sway options (thant don't working)

--- configuration.nix
+++ configuration.nix
@@ -93,7 +93,23 @@
     iosevka
   ];
 
+  # probably need for sway:
   security.polkit.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+    ];
+  };
+  
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
