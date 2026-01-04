2e4d698c75f00d0da0d164d958ae63ec2b10e1fa
Ilya Cherezov <cherezov.ia@phystech.edu>
2026-01-05 00:58:27 +0200

    use tab fzf

--- programs/fzf.nix
+++ programs/fzf.nix
@@ -8,6 +8,7 @@
   home.sessionVariables = rec {
     FZF_DEFAULT_COMMAND = "fd --type f --strip-cwd-prefix";
     FZF_CTRL_T_COMMAND = "${FZF_DEFAULT_COMMAND}";
+    FZF_DEFAULT_OPTS = "--bind tab:down,shift-tab:up";
   };
   # export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix'
   # export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
