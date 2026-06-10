0d9b5ed651c93d43f0ba17528c44eab48d232b3f
Ilya Cherezov <cherezov.ia@phystech.edu>
2026-06-10 13:58:23 +0200

    zenbook: enable sshd without autostart

--- zenbook/configuration.nix
+++ zenbook/configuration.nix
@@ -184,7 +184,8 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
+  systemd.services.sshd.wantedBy = lib.mkForce [];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
